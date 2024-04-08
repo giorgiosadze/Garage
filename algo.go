7b8f0a0fbcd224879213b7c316da2ce9dbd5f739
George Osadze <112907508+giorgiosadze@users.noreply.github.com>
2024-04-08 05:19:25 -0400

    Update algo.go

--- algo.go
+++ algo.go
@@ -9,3 +9,23 @@ func AllOf[T any](data []T, f func(T) bool) bool {
     }
     return true
 }
+
+// AnyOf checks if any elements in the slice satisfy the predicate f.
+func AnyOf[T any](data []T, f func(T) bool) bool {
+    for _, v := range data {
+        if f(v) {
+            return true
+        }
+    }
+    return false
+}
+
+// NoneOf checks if none of the elements in the slice satisfy the predicate f.
+func NoneOf[T any](data []T, f func(T) bool) bool {
+    for _, v := range data {
+        if f(v) {
+            return false
+        }
+    }
+    return true
+}
