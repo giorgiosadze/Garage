1c4d8b4676060ac6af7111a47ab854089a133281
George Osadze <osadzeg@gmail.com>
2024-11-05 19:39:26 -0500

    Create fetch-page.go

---
+++ golang/net-ex/fetch-page.go
@@ -0,0 +1,22 @@
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://osadze.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+}
